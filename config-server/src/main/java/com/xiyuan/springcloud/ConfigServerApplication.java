336c98700fdf0c8686a57b4d87518cf235209c3d
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-06-22 17:46:38 +0800

    eureka 注册完成

--- config-server/src/main/java/com/xiyuan/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/xiyuan/springcloud/ConfigServerApplication.java
@@ -1,12 +1,41 @@
 package com.xiyuan.springcloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
 public class ConfigServerApplication {
+
+    @Value("${spring.security.user.name}")
+    private String userName;
+
+    @Value("${spring.security.user.password}")
+    private String userPassword;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(userName).password("{noop}" + userPassword).roles("ADMIN");
+    }
+
+    @Configuration
+    @Order(1)
+    public class HttpBasicConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
--- eureka-server/src/main/java/com/xiyuan/springcloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/xiyuan/springcloud/EurekaServerApplication.java
@@ -1,20 +1,21 @@
-package com.xiyuan.springcloud.auth;
+package com.xiyuan.springcloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 
-/**
- * Created by xiyuan_fengyu on 2018/6/22 15:24.
- */
-@Configuration
+@EnableAutoConfiguration
+@EnableEurekaServer
 @EnableWebSecurity
-public class AuthConfig extends WebSecurityConfigurerAdapter {
+public class EurekaServerApplication {
 
     @Value("${spring.security.user.name}")
     private String userName;
@@ -22,18 +23,24 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
     @Value("${spring.security.user.password}")
     private String userPassword;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").hasRole("ADMIN")
-                .and()
-                .formLogin();
-    }
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser(userName).password("{noop}" + userPassword).roles("ADMIN");
+    }
+
+    @Configuration
+    @Order(1)
+    public class EurakaAuthConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().ignoringAntMatchers("/eureka/**");
+            http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
